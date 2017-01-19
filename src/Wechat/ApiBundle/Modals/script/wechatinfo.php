c5b8106eb6c174ab2c5025884cca8f53c6bdda07
Dirc Wang <dirk.wang@samesamechina.com>
2017-01-19 15:41:49 +0800

    create project

---
+++ src/Wechat/ApiBundle/Modals/script/wechatinfo.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'TOKEN' => 'mtu_online_wechat_samesamechina',
+  'appid' => 'wxdffb071f687633b8',
+  'secret' => '63977215c2939310e5518bd289ba65d8',
+  'prostr' => 'mtu:online:',
+  'token_time' => 'mtu:online:token_time',
+  'access_token' => 'mtu:online:access_token',
+  'access_ticket' => 'mtu:online:access_ticket',
+);
